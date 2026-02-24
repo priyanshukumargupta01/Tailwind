bddda667c8848a0b4ca539a6a3000ba164dfa44e
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-24 16:07:06 +0530

    more

---
+++ NotesApplication/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='Enter Notes Heading'/>
+        <h1 className='text-400' bg-red-900>m</h1>
+      </form>
+    </div>
+  )
+}
+
+export default App
