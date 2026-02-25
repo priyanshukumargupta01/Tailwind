4635c3e9192e697bdc49d8cec058e697438e64d9
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-26 00:59:00 +0530

    more

--- NotesApplication/src/App.jsx
+++ NotesApplication/src/App.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 const App = () => {
+
+  const sumbitHandler=()=>{
+    
+  }
   return (
     <div className="h-screen bg-black text-white">
-      <form className="flex flex-col p-10 gap-2" >
+      <form className="flex items-start justify-between p-10 gap-4" >
+        <div className="flex flex-col w-1/2 ">
         <input 
         type="text" 
-        className="px-5 py-2 border-4 rounded m-2.5"
+        className="px-5 w-full py-2 border-4 rounded m-2.5"
         placeholder="Enter Notes Heading" 
         />
-        <input 
+        <textarea 
         type="text" 
         placeholder="Write Details"
-        className="px-5 py-2 h-20 border-4 rounded m-2.5"  
+        className="px-5 w-full py-2 h-40 border-4 rounded m-2.5"  
         />
-        <button></button>
+        <button className="bg-white w-full px-4 py-2 m-2.5 rounded text-black">Add Notes</button>
+        </div>
+        <img className="h-90" src="https://static.vecteezy.com/system/resources/previews/022/299/318/non_2x/sticky-note-paper-transparent-free-png.png" alt="" />
       </form>
     </div>
   );
