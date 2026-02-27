4482321eeba293def5767e1c642d69b2135d302c
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-27 23:34:32 +0530

    More

--- NotesApplication/src/App.jsx
+++ NotesApplication/src/App.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [title, settitle] = useState("")
+
+
   const sumbitHandler = (e) => {
     e.preventDefault();
-    console.log("sumbit");
+    console.log(title);
+    settitle("")
   };
   return (
     <div className="h-screen bg-black text-white">
       <form className="lg:flex items-start justify-between p-10 gap-4">
         <div className="flex flex-col lg:w-1/2 ">
-        <h1 className="text-2xl font-semibold">Enter Notes</h1>
-        
+          <h1 className="text-2xl font-semibold">Enter Notes</h1>
+
           <input
             type="text"
             className="px-5 w-full py-2 border-4 rounded m-2.5"
             placeholder="Enter Notes Heading"
+            value={title}
+            onChange={(e)=>{
+              console.log(
+                settitle(e.target.value)
+              )
+            }}
           />
           <textarea
             type="text"
@@ -32,13 +43,24 @@ const App = () => {
         </div>
         <div className="lg:mx-30  mx-10">
           <h1 className="text-2xl font-semibold">Recent Notes</h1>
-          <div className=" flex gap-4 overflow-x-auto
- " >
-          <div className="h-42 w-35 m-5 min-w-32  rounded-2xl bg-white "> </div>
-          <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white "> </div>
-          <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white "> </div>
-          <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white "> </div>
-        </div></div>
+          <div
+            className=" flex gap-4 overflow-x-auto
+ "
+          >
+            <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white ">
+              {" "}
+            </div>
+            <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white ">
+              {" "}
+            </div>
+            <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white ">
+              {" "}
+            </div>
+            <div className="h-42 w-32  m-5 rounded-2xl min-w-32 bg-white ">
+              {" "}
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
