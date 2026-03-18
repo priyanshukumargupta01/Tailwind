3fe8e292c99ac8de8d0e85777109cd1dcb132086
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-18 18:19:54 +0530

    more

--- GalleryProject/src/App.jsx
+++ GalleryProject/src/App.jsx
@@ -1,23 +1,38 @@
-import axios from "axios"
-
-
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [userData, setUserData] = useState([]);
 
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=10",
+    );
 
- const getData = ()=>{
-  axios.post("https://picsum.photos/v2/list?page=2&limit=10",{user:"pk",password:"12345678"})
-  
- }
+    setUserData(response.data);
+  };
+  let presentData = "Data not founded";
 
   return (
-    <div className='bg-black h-screen p-4 text-white'>
-      <button 
-      onClick={getData}
-      className="bg-green-600 active:scale-95 text-white px-2 py-4 rounded "
-      >Get DATA</button>
+    <div className="bg-black h-full p-4 text-white">
+      <button
+        onClick={getData}
+        className="bg-green-600 active:scale-95 text-white px-2 py-4 rounded"
+      >
+        Get DATA
+      </button>
+      <div className="flex  justify-evenly overflow-x-hidden p-5 ">
+        {userData.map((item, index) => (
+          <img
+            key={index}
+            src={item.download_url}
+            alt=""
+            className="w-40 mb-2 p-6"
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
