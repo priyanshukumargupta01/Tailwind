20ca0e52910adbab5de07acf15786e723c94a1d5
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-18 12:49:21 +0530

    installing api data

--- GalleryProject/src/App.jsx
+++ GalleryProject/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import axios from "axios"
+
+
 
 const App = () => {
+
+
+ const getData = ()=>{
+  axios.post("https://picsum.photos/v2/list?page=2&limit=10",{user:"pk",password:"12345678"})
+  
+ }
+
   return (
-    <div>App</div>
+    <div className='bg-black h-screen p-4 text-white'>
+      <button 
+      onClick={getData}
+      className="bg-green-600 active:scale-95 text-white px-2 py-4 rounded "
+      >Get DATA</button>
+    </div>
   )
 }
 
